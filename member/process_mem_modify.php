f5e58c9f52ce363f7cc56aacfa209eed44f03704
DESKTOP-7DM8FKK\mx02-1 <kimmy100b@gmail.com>
2020-08-05 16:25:14 +0900

    회원정보 수정페이지 완성

--- member/process_mem_modify.php
+++ member/process_mem_modify.php
@@ -1,2 +1,30 @@
 <?php
+    header('Content-Type: text/html; charset=utf-8');
+    session_start();
+   
+    require_once '../DB.php';
+    $conn = db_connect();
+
+    $user=$_SESSION['memberId'];
+    $pw = $_POST['memberPw'];
+    $name = $_POST['memberName'];
+    $tel = $_POST['memberTel'];
+    $email = $_POST['memberEmail'];
+
+    $sql = "UPDATE member SET passwd = '".$pw."', tel='".$tel."', email = '".$email."' WHERE id= '".$user."'";
+    $result = mysqli_query($conn, $sql);
+
+    if($result){ ?>
+        <script>
+            alert("저장되었습니다");
+            location.href="../list.php";
+        </script>
+    <?php        
+    }else{ ?>
+        <script>
+            alert("실패하였습니다.");
+            history.back();
+        </script>
+    <?php
+    }
 ?>
