739e22006f1160d78550af124d44f583a39fae78
DESKTOP-7DM8FKK\mx02-1 <kimmy100b@gmail.com>
2020-08-06 10:31:35 +0900

    로그인 페이지 수정 - 이뿌게

--- login/login_form.php
+++ login/login_form.php
@@ -7,32 +7,26 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
-    <link rel="stylesheet" href="../css/signupFormStyle.css">
+    <link rel="stylesheet" href="../css/loginFormStyle.css">
     <title>로그인</title>
 </head>
-<body>
-<?php
-        if(isset($_COOKIE["memberId"])){
-            $_SESSION["memberId"]=$_COOKIE["memberId"];
-        }
+<body class="align">
+  <?php
+          if(isset($_COOKIE["memberId"])){
+              $_SESSION["memberId"]=$_COOKIE["memberId"];
+          }
 
-        if(!isset($_SESSION["userid"])) {
-    ?>
-<form action="login_result.php" method="POST">
-  <div class="form-group">
-    <label for="exampleInputEmail1">아이디</label>
-    <input type="text" class="form-control" name="memberId" placeholder="아이디" aria-describedby="emailHelp" required>
+          if(!isset($_SESSION["userid"])) {
+      ?>
+  <div class="grid">
+  <div class="wrapper">
+    <form class="form-signin" action="login_result.php" method="POST">       
+      <h2 class="form-signin-heading">Please login</h2>
+      <input type="text" class="form-control" name="memberId" placeholder="아이디" required="" autofocus="" />
+      <input type="password" class="form-control" name="memberPw" placeholder="비밀번호" required=""/>      
+          <button class="btn btn-lg btn-primary btn-block" type="submit">Login</button>   
+    </form>
   </div>
-  <div class="form-group">
-    <label for="exampleInputPassword1">비밀번호</label>
-    <input type="password" class="form-control" name="memberPw" placeholder="비밀번호" required>
-  </div>
-  <div class="form-group form-check">
-    <input type="checkbox" class="form-check-input" name="chbox">
-    <label class="form-check-label" for="exampleCheck1">로그인 상태유지</label>
-  </div>
-      <button type="submit" class="btn btn-secondary btn-login">로그인</button>   
-</form>
 <?php
         } else{
     ?>
@@ -41,6 +35,5 @@
 <?php
         }
     ?>
-
 </body>
 </html>
