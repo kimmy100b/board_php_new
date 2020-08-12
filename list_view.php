ec70199f8947b462ed4206afe0060ec303c7e6e9
DESKTOP-7DM8FKK\mx02-1 <kimmy100b@gmail.com>
2020-08-12 17:06:39 +0900

    이전 다음 css

--- list_view.php
+++ list_view.php
@@ -34,13 +34,13 @@ $f_name = $file['name'];
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
-    <link rel="stylesheet" href="./css/listViewStyle.css">
+    <link rel="stylesheet" href="css/listViewStyle.css">
     <title>게시판</title>
 </head>
 <body>
     <div class="move">
       <a href="list_view.php?id=<?= ($id-1) ?> ">< 이전 게시물</a>
-      <a href="list_view.php?id=<?= ($id+1) ?>">다음 게시물 ></a>
+      <a href="list_view.php?id=<?= ($id+1) ?>" class="move__next">다음 게시물 ></a>
     </div>
     <article class="article">
     <table class="table table-bordered">
