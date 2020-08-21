7625d2e0551ea65d3dbf17f91c3a86a80f2a254e
kimmy100b <kimmy100b@gmail.com>
2020-08-21 11:04:35 +0900

    게시판 프로젝트 보수작업

--- index.php
+++ index.php
@@ -1,103 +1,14 @@
 <?php
-include_once "DBconnect.php";
+# 메인 화면
+
+// DB연동
+// require_once "DB.php";
+// $conn = db_connect();
+
+session_start();
+
+include "include/header.php";
 ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <script src="https://kit.fontawesome.com/2a001071af.js" crossorigin="anonymous"></script>
-    <link rel="stylesheet" href="css/index.css">
-    <title>Document</title>
-</head>
-
 <body>
-    <nav>
-        <ul>
-            <li><a href="#">About</a></li>
-            <li><a href="list.php">게시판</a></li>
-            <li><a href="#">마이페이지</a></li>
-        </ul>
-        <div class="login">
-            <?php 
-            if(isset($_SESSION['memberId'])){ ?>
-            <p class="login__msg"><?php echo $_SESSION['memberId'];?>님 안녕하세요  </p> <button class="btn btn-outline-secondary logout__btn" onclick="location.href='login/logout.php'">로그아웃</button>
-
-            <?php
-            }else{
-                ?>
-            <a href="login/login_form.php">로그인</a>
-            <a href="signup/signup_form.php">회원가입</a>
-            <?php
-            }
-            ?>
-        </div>
-        <div class="button">
-            <a class="btn-open" href="#"></a>
-        </div>
-    </nav>
-        <div class="overlay">
-            <div class="wrap">
-                <ul class="wrap-nav">
-                    <li><a href="#">About</a>
-                    <ul>
-                        <li><a href="#">About Company</a></li>
-                        <li><a href="#">Designers</a></li>
-                        <li><a href="#">Developers</a></li>
-                        <li><a href="#">Pets</a></li>
-                    </ul>
-                    </li>
-                    <li><a href="#">Services</a>
-                    <ul>
-                        <li><a href="https://www.google.hr/">Web Design</a></li>
-                        <li><a href="#">Development</a></li>
-                        <li><a href="#">Apps</a></li>
-                        <li><a href="#">Graphic design</a></li>
-                        <li><a href="#">Branding</a></li>
-                    </ul>
-                    </li>
-                    <li><a href="#">Work</a>
-                    <ul>
-                        <li><a href="#">Web</a></li>
-                        <li><a href="#">Graphic</a></li>
-                        <li><a href="#">Apps</a></li>
-                    </ul>
-                    </li>
-                </ul>
-                <div class="social">
-                    <a href="http://mario-loncarek.from.hr/">
-                    <div class="social-icon">
-                        <i class="fa fa-facebook"></i>
-                    </div>
-                    </a>
-                    <a href="#">
-                    <div class="social-icon">
-                        <i class="fa fa-twitter"></i>
-                    </div>
-                    </a>
-                    <a href="#">
-                    <div class="social-icon">
-                        <i class="fa fa-codepen"></i>
-                    </div>
-                    </a>
-                    <a href="#">
-                    <div class="social-icon">
-                        <i class="fa fa-behance"></i>
-                    </div>
-                    </a>
-                    <a href="#">
-                    <div class="social-icon">
-                        <i class="fa fa-dribbble"></i>
-                    </div>
-                    </a>
-                    <p>
-                        From: Zagreb, Croatia<br>
-                         Site: <a href="http://mario-loncarek.from.hr/">mario-loncarek.from.hr</a>
-                    </p>
-                </div>
-            </div>
-        </div>
+<h1>메인화면</h1>  
 </body>
-<script src="https://code.jquery.com/jquery-1.9.1.min.js"></script>
-<script src="js/index.js"></script>
-</html>
